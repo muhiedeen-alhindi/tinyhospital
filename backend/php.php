814fb01e1d89e94311def9c6003cdf4e21a39467
muhiedeen <muhiedeen.alhindi@gmail.com>
2023-03-11 17:30:10 +0200

    home page and signup html

---
+++ backend/php.php
@@ -0,0 +1,12 @@
+<?php 
+
+ 
+ 
+include('connection.php');
+
+
+ 
+
+ 
+
+?>
---
+++ frontend/js.js
@@ -0,0 +1,47 @@
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+ // 
+  // let container =document.getElementById("container")
+
+
+// let html=''   
+// const init = () => {
+
+//     axios({
+//         method:"get",
+//         url : "http://localhost/jquery/backend/php.php/ "
+//       }).then(function (res) {
+//         console.log(res.data);
+//         for(i = 0; i < res.data.length; i++){
+//             let id = res.data[i].id;
+//             // let image = res.data[i].image;
+//             let title = res.data[i].title;
+//             let content = res.data[i].content;
+//             let auther =res.data[i].auther;
+//             let published_date=res.data[i].published_date;
+//               html="<div><div>"
+//               html +=`<div> <p> ${id}</p> <div>`
+           
+//             container.insertAdjacentHTML('beforeend', html);
+//         }
+       
+        
+//       })
+  
+//     }
+
+// init();
