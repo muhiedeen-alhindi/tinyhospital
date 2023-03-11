63270473dc459c42ec7907db9a44bf3162f6273d
muhiedeen <muhiedeen.alhindi@gmail.com>
2023-03-11 18:42:31 +0200

    added api signup

---
+++ backend/signup.php
@@ -0,0 +1,29 @@
+<?php
+include('connection.php');
+
+header('Access-Control-Allow-Origin:*');
+$email = $_POST['email'];
+$name = $_POST['name'];
+$dob = $_POST['dob'];
+$password = $_POST['password'];
+$type = $_POST['type'];
+
+$signup =  $mysqli->prepare('select email from users where email=?');
+$signup ->bind_param('s', $email);
+$signup ->execute();
+$signup ->store_result();
+$check_signup  = $signup  ->num_rows();
+
+$hashed_password = password_hash($password, PASSWORD_BCRYPT);
+
+if ($check_signup > 0) {
+    $response['status'] = "failed";
+} else {
+    $query =   $mysqli->prepare('insert into users(name,email,password,dob,type) values(?,?,?,?,?)');
+    $query->bind_param('ssssi', $name, $email, $hashed_password,$dob,$type);
+    $query->execute();
+    $response['status'] = "success";
+}
+
+echo json_encode($response);
+?>
